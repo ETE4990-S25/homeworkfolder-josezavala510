241ad0ff739cc64ae416db629e960a5d00510324
Jose Zavala <josezavala@cpp.edu>
2025-02-08 00:28:44 -0800

    Lab 3

---
+++ Labs/Names.py
@@ -0,0 +1,16 @@
+
+        
+
+
+names_list = ["Josh", "David", "Marc"]
+
+def greet_user(name):
+    """Greet the user and check if the name is in the list."""
+    
+    if name in names_list:
+        print(f"{name} is on the list. Good job!")
+    else:
+        print("Sorry, the name is not popular.")
+
+
+
