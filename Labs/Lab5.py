99346ca5f9148c0f1698f9a6d020bbdfcad4d5cd
Jose Zavala <josezavala@cpp.edu>
2025-03-12 08:35:26 -0700

    JoseZavala_Homework05_.py

---
+++ Labs/Lab5.py
@@ -0,0 +1,20 @@
+class Sage(object):
+   # """Wise Healer charecter to help"""
+    def __init__(self, name, variety, health, healing=100 ):
+       # """Initialize the name, variety, lifesf healer attributes."""
+        self.name = name
+        self.variety = variety
+        self.health = health
+        self.healing = healing 
+    def heal(self):
+        return self.name + " heals " + str(self.healing)
+        #"""Heal a certain amount of life."""
+    #self.lifes += amount
+
+    
+    
+sage = Sage("Eagus","Mystical", 150)
+print(sage.heal())
+
+
+    
